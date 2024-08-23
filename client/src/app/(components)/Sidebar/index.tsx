266fbef041a38816278e9bc030546497f07c9748
Siddharth Tiwari <tiwarisiddharth27@gmail.com>
2024-08-23 13:07:27 +0530

    added images

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -11,6 +11,7 @@ import {
   SlidersHorizontal,
   User,
 } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
@@ -74,7 +75,13 @@ const Sidebar = () => {
         className={`flex gap-3 justify-between md:justify-normal items-center pt-8
           ${isSidebarCollapsed ? "px-5" : "px-8"}`}
       >
-        <div>logo</div>
+        <Image
+          src="https://3-inventorymanagement-744.s3.ap-south-1.amazonaws.com/logo.png"
+          alt="stock-logo"
+          width={27}
+          height={27}
+          className="rounded w-8"
+        />
         <h1
           className={`${
             isSidebarCollapsed ? "hidden" : "block"
